fb9c4b0416f8ef3581b6a7c96c415cb2e8d70c43
shinsegae <zergod@nate.com>
2018-07-24 01:35:49 +0900

    메모 수정

--- src/com/segae/memo/Memo.java
+++ src/com/segae/memo/Memo.java
@@ -1,10 +1,8 @@
 package com.segae.memo;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,79 +11,145 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Memo {
-	
 	public static final String MEMO_DIR = "/temp/memo/";
-
-	//종료 커맨드를 상수로 정의 
+	
 	public static final String EXIT = "/exit";
 	
-	//생성장에서 메모를 저장할 디렉토리를 생성해준다.
 	public Memo() {
 		File dir = new File(MEMO_DIR);
-		if(!dir.exists()) {
+		if(dir.exists()) {
 			dir.mkdirs();
 		}
 	}
-
-	// 1 . 명령어를 출력하는 함수
+	
 	public void showCommand() {
-		System.out.println("아래 명령 번호를 입력하세요---");
-		System.out.println("1. 쓰기. 2. 읽기. 3. 수정. 4. 삭제. 0. 프로그램종료.");
+		System.out.println("아래명령 번호를 입력하세요-----");
+		System.out.println("1.쓰기 2.읽기 3.수정 4.삭제 0.종료");
 	}
 
-	// 쓰기
 	public void write(Scanner scanner) {
-		System.out.println("--- 쓰기 모드---");
-		//전체글을 저장할 변수
+		System.out.println("-----쓰기모드------");
 		StringBuilder content = new StringBuilder();
-		
-		// 키보드 입력을 받아야 한다. 
 		while(true) {
-			String line = scanner.nextLine();
-			if(line.equals(EXIT)) {
-				break;
-			}else {		
-				content.append(line + "\r\n");
-			}
+		String line = scanner.nextLine();
+		if(line.equals(EXIT)) {
+			break;
+		}else {
+			content.append(line + "\r\n");
 		}
-		//작성한 내용이 있으면 파일로 쓴다.
+	  }
+	
 		if(!content.toString().equals("")) {
-			//가. 현재 시간 가져와서 파일명으로 만든다.
 			long now = System.currentTimeMillis();
-			//나. 년월일_시분초.txt 파일로 포맷팅
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyMMdd_hhmmss");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_hhmmss");
 			String filename = sdf.format(now) + ".txt";
-			//다. 내용을 저장할 파일경로 설정
+			// 내용을 저장할 파일경로 설정
 			Path path = Paths.get(MEMO_DIR, filename);
 			try {
-			//라. 파일쓰기
-			Files.write(path, content.toString().getBytes());
+			// 파일쓰기	
+				Files.write(path, content.toString().getBytes());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println("메모를 등록하였습니다.");
-			}
-	}
-	
-	// 파일 목록 보기
-	public void list() {
-		File file = new File(MEMO_DIR);
-		String fileList[] = file.list();
-		for(String filename : fileList) {
-			System.out.println(filename);
+			System.out.println("메모를 등록하였습니다");
 		}
+	
 	}
 
-	public void remove() {
+	public void list() {
+
 		File file = new File(MEMO_DIR);
-		
-		if( file.exists()) {
-	    String[] fileList = file.list();        
-	    for(int i=0; i<fileList.length; i++) {
-	    	   System.out.println(fileList[i]); 
-	            File tempFile= new File(MEMO_DIR +"\\"+ fileList[i]); 
-	           tempFile.delete(); 
-	    	}    
-	    }       
-	}	    
-}  
+		String[] list = file.list();
+
+		for (String lists : list) {
+			System.out.println(lists);
+		}
+		System.out.println("");
+	}
+	
+	public void read(Scanner scanner) {
+
+		String result = "";
+
+		list(); 
+
+		System.out.println("---파일 목록---");
+		System.out.println("파일목록을 선택하세요 ");
+		String name = scanner.nextLine();
+		String filePath = MEMO_DIR.concat(name);
+
+		File file = new File(filePath);
+		if (file.exists()) {
+			try (FileReader fr = new FileReader(file)) { 
+			  int oneChar = 0; 
+			  	while (true) {
+			  		oneChar = fr.read();
+			  	if (oneChar == -1) { 
+						break; 
+			  	} else {
+						result = result + (char) oneChar; 
+					}
+				}
+			  	System.out.println(result);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+				} else {
+					System.out.println("파일이름을 잘못 입력하였습니다.");
+		}
+
+	
+	}
+	
+		public void modify(Scanner scanner) {
+		list(); 
+			System.out.println("---파일 목록---");
+			System.out.println("파일을 선택하세요");
+		String name = scanner.nextLine(); 
+		String filePath = MEMO_DIR.concat(name);
+			File file = new File(filePath);
+		if (!file.delete()) { 
+			}
+			System.out.println("수정 할 내용을 입력하세요");
+			StringBuilder content = new StringBuilder();
+		while (true) {
+			String line = scanner.nextLine();
+		if (line.equals(EXIT))
+				break;
+			else {
+				content.append(line + "\r\n");
+			}
+		}
+			if (!content.toString().equals("")) {
+				Path path = Paths.get(filePath);
+			try {
+				Files.write(path, content.toString().getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("수정 저장 되었습니다");
+
+	}
+
+		public void remove(Scanner scanner) {
+			list(); // 
+			System.out.println("---파일 목록---");
+			System.out.println("파일을 선택하세요 ");
+			String name = scanner.nextLine(); 
+			String filePath = MEMO_DIR.concat(name); 
+		File file = new File(filePath);
+			if (!file.delete()) { 
+				System.out.println("다시 선택해주세요");
+			} else {
+				System.out.println("삭제완료");}
+			list(); 
+
+			
+		}
+
+
+
+}
--- src/com/segae/memo/MemoMain.java
+++ src/com/segae/memo/MemoMain.java
@@ -5,45 +5,39 @@ import java.util.Scanner;
 public class MemoMain {
 
 	public static void main(String[] args) {
-		
-		// 1. 키보드 입력으로 명령어를 받는다. 
-		// + 프로그램 시작 하면 명령어 번호를 보여준다. 
-		// + 1. 쓰기 2. 읽기 3. 수정 4. 삭제 
+
 		Memo memo = new Memo();
-		memo.showCommand();
 		
-		Memo2 memo2 = new Memo2();
 		
 		Scanner scanner = new Scanner(System.in);
-		
-		boolean runFlag = true;
+		boolean runFlag =true;
 		
 		while(runFlag) {
-		String cmd = scanner.nextLine();
-	
-		switch(cmd) {
-			case "1" : // 쓰기
-				memo.write(scanner);
-				break;
-			case "2" :
-				memo.list();
-				break;
-			case "3" :
-				//memo.rewrite();
-				memo2.read("C:\\Temp\\memo\\20180721_090702.txt");
-				break;
-			case "4" :
-				memo.remove();
-				break;
-			case "0" :
-				runFlag = false;				
-				break;
-			default : 
-				System.out.println("명령어가 잘못되었습니다 !");
-		}
-		}
+			memo.showCommand();
+			String command = scanner.nextLine();
 		
-		System.out.println("프로그램이 종료되었습니다!");
+		switch(command) {
+		case"1": 
+			memo.write(scanner);
+			break;
+		case"2":
+			memo.read(scanner);
+			break;
+		case"3":
+			memo.modify(scanner);
+			break;
+		case"4":
+			memo.remove(scanner);
+			break;
+		case"0":
+			runFlag = false ; 
+			break;
+			default : 
+				System.out.println("명령어가 잘못 되었습니다");
+			}
+		}
+		System.out.println("프로그램이 종료 되었습니다");
+		
 	}
-
+	
 }
